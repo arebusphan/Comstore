1c2b6950319b55f49b6364a5114b3e8e4660dd9b
Dung <dungarebus@gmail.com>
2026-05-19 11:13:07 +0700

    AIAdviceRepository

---
+++ Repository/AIAdviceRepository.cs
@@ -0,0 +1,31 @@
+﻿namespace Repository;
+using Data;
+using Microsoft.EntityFrameworkCore;
+public class AIAdviceRepository
+{
+private readonly ComStoreDbContext _context;
+public AIAdviceRepository(ComStoreDbContext context)
+{
+    _context = context;
+}
+public async Task<AICounseRequest> AddAICounseRequest(AICounseRequest request)
+{
+    _context.AICounseRequests.Add(request);
+    await _context.SaveChangesAsync();
+    return request;
+}
+public async Task<IEnumerable<AICounseRequest>> GetHistoryAICounseRequest(int userId)
+{
+    return await _context.AICounseRequests
+    .Where(r => r.UserId == userId)
+    .OrderByDescending(r => r.CreatedAt)
+    .ToListAsync();
+}
+public async Task<IEnumerable<Product>> GetProductByRecommendation(string usageType, int budget, string preferences)
+{
+    return await _context.Products
+    .Where(p => p.Description.Contains(usageType) && p.Price <= budget && p.Brand.Contains(preferences))
+    .OrderByDescending(p => p.CreatedAt)
+    .ToListAsync();
+}
+}
