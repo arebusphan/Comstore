ff117410c8165da8426820026ba02c972905a756
Dung <dungarebus@gmail.com>
2026-05-19 10:53:01 +0700

    ProductRepository

---
+++ Repository/ProductRepository.cs
@@ -0,0 +1,45 @@
+﻿namespace Repository;
+using Data;
+using Microsoft.EntityFrameworkCore;
+public class ProductRepository
+{
+private readonly ComStoreDbContext _context;
+
+public ProductRepository(ComStoreDbContext context)
+{
+    _context = context;
+}
+public async Task<IEnumerable<Product>> GetAllProducts()
+{
+    return await _context.Products.ToListAsync();
+}
+public async Task<Product> GetProductById(int id)
+{
+    return await _context.Products.FindAsync(id);
+}
+public async Task<Product> AddProduct(Product product)
+{
+    _context.Products.Add(product);
+    await _context.SaveChangesAsync();
+    return product;
+}
+public async Task<Product> UpdateProduct(int id, Product product)
+{
+    var existingProduct = await GetProductById(id);
+    _context.Update(existingProduct);
+    await _context.SaveChangesAsync();
+    return existingProduct;
+}
+public async Task<bool> DeleteProduct(int id)
+{
+    var product = await GetProductById(id);
+    _context.Products.Remove(product);
+    await _context.SaveChangesAsync();
+    return true;
+}
+public async Task<IEnumerable<Product>> CompareProducts(List<int> ids){
+    var products = await _context.Products.Where(p => ids.Contains(p.Id)).ToListAsync();
+    return products;
+}
+}
+
--- Repository/obj/Debug/net9.0/Repository.AssemblyInfo.cs
+++ Repository/obj/Debug/net9.0/Repository.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Repository")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fb20ba9a04229042ef9c87db68d38028ed1e0d3b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d02c8cd708f10749eebfdf9ce320f8ae7a8f4a16")]
 [assembly: System.Reflection.AssemblyProductAttribute("Repository")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Repository")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
